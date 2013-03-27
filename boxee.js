b8cb9272b69b39aa34da7dc789cc776b948f5da1
Sexar <iconfidence@gmail.com>
2013-03-26 21:43:06 -0600

    Stub of centerItem, resizeImage, trimText, scaleImage methods

---
+++ boxee.js
@@ -0,0 +1,46 @@
+
+/**
+ * Description for boxee.
+ */
+
+var boxee = {
+	
+	/**
+	 * Description for centerItem.
+	 *
+	 * @param {jQuery} item
+	 */
+	centerItem: function(item){
+
+	},
+
+	/**
+	 * Description for resizeImage.
+	 *
+	 * @param {jQuery} image
+	 */
+	resizeImage: function(image){
+
+	},
+
+	/**
+	 * Description for trimText.
+	 *
+	 * @param {string} string
+	 * @param {int} size
+	 * @return 
+	 */
+	 trimText: function(){
+
+	 },
+
+	/**
+	 * Description for scaleImage.
+	 *
+	 * @param {jQuery} image
+	 */
+	 scaleImage: function(image){
+
+	 }
+
+}
